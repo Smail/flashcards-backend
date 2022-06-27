564cc5307d386b921332d7c13eba49da6f16649f
Smail <44433524+Smail@users.noreply.github.com>
2022-06-28 01:48:37 +0200

    feat: display db users (still buggy)

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const dotenv = require("dotenv");
+const sqlite3 = require("sqlite3").verbose();
+
+dotenv.config();
+
+console.log("../" + process.env.DB_NAME);
+const db = new sqlite3.Database("../" + process.env.DB_NAME);
+console.log(db);
 
 /* GET users listing. */
-router.get("/", function(req, res, next) {
-  res.send("respond with a resource");
+router.get("/", function (req, res, next) {
+  db.serialize(() => {
+    const users = [];
+
+    db.each("SELECT rowid AS id, username, firstname, lastname FROM users", function (err, row) {
+      console.log(row);
+      if (err != null) {
+        users.push({
+          id: row.id,
+          username: row.username,
+          firstname: row.firstname,
+          lastname: row.lastname,
+        });
+      } else {
+        console.error(err);
+      }
+    });
+  });
+
+  res.send(JSON.stringify(users));
 });
 
 module.exports = router;
